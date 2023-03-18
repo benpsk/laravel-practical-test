915031b7c13b35c90752f683af7586dff37de283
Ben Gunn <bengunn.dev@gmail.com>
2023-03-19 03:17:19 +0630

    setup database

---
+++ app/Models/SurveyForm.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class SurveyForm extends Model
+{
+    use HasFactory;
+
+    /**
+     * @var string[]
+     */
+    protected $fillable = ['name', 'phone_no', 'dob', 'gender', 'user_id'];
+}
