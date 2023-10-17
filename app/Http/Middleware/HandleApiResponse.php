3b75a9d061474dec79a602787107fb79157695bf
Ben Gunn <bengunn.dev@gmail.com>
2023-10-17 20:12:30 +0630

    add: prettierrc

--- app/Http/Middleware/HandleApiResponse.php
+++ app/Http/Middleware/HandleApiResponse.php
@@ -7,25 +7,24 @@ use Closure;
 use Illuminate\Http\JsonResponse;
 use Illuminate\Http\Request;
 use Symfony\Component\HttpFoundation\Response;
-
 class HandleApiResponse
 {
     public function handle(Request $request, Closure $next): Response
     {
         $start = microtime(true);
         $api = Formatter::factory()->setStart($start);
-
         $response = $next($request);
-
-        if ($response instanceof JsonResponse && $request->expectsJson() && $response->isSuccessful()) {
-
+        if ($this->isApi($request, $response)) {
             $statusCode = $response->getStatusCode();
-
             $data = $response->getData();
             return $api->make($data, $statusCode);
-
         }
-
         return $response;
+    }
+    protected function isApi($request, $response): bool
+    {
+        return $response instanceof JsonResponse &&
+            $request->expectsJson() &&
+            $response->isSuccessful();
     }
 }
